7f2978af4fb29e0bdbe5ccee7745549757ad47d3
pedro-martineli <theboguz@pop-os.localdomain>
2023-02-18 20:59:13 -0300

    Lexer String Support

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-    lexer_T* lexer = init_lexer("hello = helloworld");
+    lexer_T* lexer = init_lexer("hello = 'helloworld'");
     lexer_collect_tokens(lexer);
     parser_T* parser = init_parser(lexer->token_list);
     parser_parse(parser);
