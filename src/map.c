993fb4cb01f892a60068932654897f0aec060490
Pedro Martineli <martinelipedro@gmail.com>
2023-02-19 13:06:40 -0300

    Hash Map Implemented

---
+++ src/map.c
@@ -0,0 +1,48 @@
+#include "map.h"
+
+#include <stdio.h>
+
+map_T* init_map(size_t values_size)
+{
+    map_T* map = calloc(1, sizeof(struct MAP_STRUCT));
+
+    map->keys = init_dynamic_array(KEY_STRING_SIZE * sizeof(char));
+    map->values = init_dynamic_array(values_size);
+    map->len = 0;
+
+    return map;
+}
+
+void map_add(map_T* map, char* key, void* values)
+{
+    dynamic_array_push(map->keys, (void*)key);
+    dynamic_array_push(map->values, (void*)values);
+    map->len += 1;
+}
+
+void* map_get(map_T* map, char* key)
+{
+    for (size_t i = 0; i < map->len; i++)
+    {
+        if ((char*)map->keys->values[i] == key)
+        {
+            return map->values->values[i];
+        }
+    }
+
+    printf("[ERROR]: Value not found in map.");
+    exit(1);
+}
+
+bool map_has_key(map_T* map, char* key)
+{
+    for (size_t i = 0; i < map->len; i++)
+    {
+        if ((char*)map->keys->values[i] == key)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
---
+++ src/map.h
@@ -0,0 +1,23 @@
+#ifndef MAP_H
+#define MAP_H
+
+#include <stdbool.h>
+#include "dynamic_array.h"
+
+#define KEY_STRING_SIZE 30
+
+typedef struct MAP_STRUCT
+{
+    dynamic_array_T* keys;
+    dynamic_array_T* values;
+    size_t len;
+
+} map_T;
+
+map_T* init_map(size_t values_size);
+void map_add(map_T* map, char* key, void* values);
+void* map_get(map_T* map, char* key);
+bool map_has_key(map_T* map, char* key);
+
+
+#endif
